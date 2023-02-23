2b96c8f117be98f157ead7864db136f17fd1bfa1
lucifermorningstar1311 <sharmaparshuram2004@gmail.com>
2023-02-23 23:26:20 +0530

    normal addition

---
+++ code.java
@@ -0,0 +1,12 @@
+class code {
+    public static void main(String[] args) {
+
+        {
+            int a = 10;
+            int b = 12;
+            int sum = a + b;
+            System.out.println(sum);
+
+        }
+    }
+}
